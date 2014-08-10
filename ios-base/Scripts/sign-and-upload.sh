765f5108599d84a4cf2e3cd43a69a56564f0d348
nikitafomin <nikitafomin89@gmail.com>
2014-08-10 22:13:40 +0400

    change build description, new distribution list on testflight

--- ios-base/Scripts/sign-and-upload.sh
+++ ios-base/Scripts/sign-and-upload.sh
@@ -11,24 +11,28 @@ fi
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
+echo ""
 echo "********************"
 echo "* Signing *"
 echo "********************"
+echo ""
 
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dSYM.zip" "$OUTPUTDIR/$APP_NAME.app.dSYM"
 
+echo ""
 echo "********************"
 echo "* Uploading *"
 echo "********************"
+echo ""
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
-RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+RELEASE_NOTES="This version was uploaded automagically by Travis\nTravis Build number $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
 
 curl http://testflightapp.com/api/builds.json \
 -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
 -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
 -F api_token="$TESTFLIGHT_API_TOKEN" \
 -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
--F distribution_lists='Internal' \
+-F distribution_lists='developers' \
 -F notes="$RELEASE_NOTES"
