db717981af9890d948d153083947cb75233ec4b2
nikitafomin <nikitafomin89@gmail.com>
2014-08-10 21:38:34 +0400

    add creating .dSYM.zip file in scripts

--- ios-base/Scripts/sign-and-upload.sh
+++ ios-base/Scripts/sign-and-upload.sh
@@ -11,7 +11,16 @@ fi
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
+echo "********************"
+echo "* Signing *"
+echo "********************"
+
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+zip -r -9 "$OUTPUTDIR/$APPNAME.app.dSYM.zip" "$OUTPUTDIR/$APPNAME.app.dSYM"
+
+echo "********************"
+echo "* Uploading *"
+echo "********************"
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
