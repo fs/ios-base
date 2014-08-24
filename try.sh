50adb430970c645836dcfd8720b4e6f94919923a
nikitafomin <nikitafomin89@gmail.com>
2014-08-24 22:54:43 +0400

    remove try.sh file

--- try.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# xcodebuild -workspace ios-base.xcworkspace -scheme ios-base-Production -sdk iphoneos -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO | xcpretty -c && exit ${PIPESTATUS[0]}
-
-xcodebuild -workspace ios-base.xcworkspace -scheme ios-base-Staging -destination 'platform=iOS Simulator,name=iPhone Retina (4-inch),OS=7.1' test | xcpretty -c && exit ${PIPESTATUS[0]}
-
-
